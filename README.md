c2279544c14181f6eaa1bc941a6b57bb410e8476
dustinbleile <dustinbleile@gmail.com>
2020-08-05 23:45:12 -0700

    final updates

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Created from the wagtail cookiecutter
 
 cookiecutter https://github.com/torchbox/cookiecutter-wagtail.git
 
+pip install -U -e .[dev] --use-feature=2020-resolver
--- webwonking/settings/base.py
+++ webwonking/settings/base.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     "webwonking.home",
     "webwonking.search",
-    "wagtail.contrib.wagtailsearchpromotions",
+    "wagtail.contrib.search_promotions",
     "wagtail.wagtailforms",
     "wagtail.wagtailredirects",
     "wagtail.wagtailembeds",
